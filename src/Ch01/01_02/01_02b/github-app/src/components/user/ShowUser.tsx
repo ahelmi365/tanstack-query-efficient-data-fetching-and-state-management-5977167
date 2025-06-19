362620fc94670067f61477ad51df52420ceb442c
Ali Helmi <ahelmi365@gmail.com>
2025-06-19 07:45:41 +0300

    refactor: optimize user data fetching with useQueries in ShowUser component

--- src/Ch01/01_02/01_02b/github-app/src/components/user/ShowUser.tsx
+++ src/Ch01/01_02/01_02b/github-app/src/components/user/ShowUser.tsx
@@ -1,30 +1,36 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries, useQuery } from "@tanstack/react-query";
 import { getUser } from "../../apis/user"
 import UserCard from "../../ui/userCard/UserCard";
 
 const ShowUser = () => {
-  const username1 = "ahelmi365";
-  const username2 = "octocat";
-  const user1 = useQuery({
-    queryKey: ['user',  username1],
-    queryFn: () => getUser(username1),
-  });
 
-  const user2 = useQuery({
-    queryKey: ['user', username2],
-    queryFn: () => getUser(username2),
-  });
-
-  if (user1.isLoading || user2.isLoading) {
+  const usernames = ["ahelmi365", "octocat"];
+  const results = useQueries({
+    queries: usernames.map(username => {
+      return {
+        queryKey: ['user', username],
+        queryFn: () => getUser(username),
+        cacheTime: 1000 * 60 * 5 // cache for 5 minutes
+      }
+    })
+  })
+  const isLoading = results.some(result => result.isLoading);
+  const isError = results.some(result => result.isError);
+  if (isLoading) {
     return <div>Loading...</div>
   }
-  if (user1.isError || user2.isError) {
+  if (isError) {
     return <div>Error loading user data</div>
   }
+
+
   return (
     <div>
-      <UserCard user={user1.data} />
-      <UserCard user={user2.data} />
+      {results.map((result, index) => {
+
+        return <UserCard key={index} user={result.data} />
+      }
+      )}
     </div>
   );
 
